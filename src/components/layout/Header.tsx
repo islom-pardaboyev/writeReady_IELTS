747e14edc031d869c8191030c3247e3656835b47
Islom Pardaboyev <ipardaboyev574@gmail.com>
2026-06-27 11:42:23 +0500

    updated header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,12 +6,10 @@ export function Header() {
   const { user } = useAuth();
 
   const firstName = user?.displayName
-    ? user.displayName.split(' ')[0]
-    : user?.email?.split('@')[0] ?? '';
 
   return (
     <header
-      className="sticky top-0 z-[100] bg-[rgba(255,255,255,0.95)] backdrop-blur-[8px] border-b border-[#e2e8f0]"
+      className="sticky top-0 py-2 z-[100] bg-[rgba(255,255,255,0.95)] backdrop-blur-[8px] border-b border-[#e2e8f0]"
     >
       <div
         className="max-w-[1160px] mx-auto px-6 h-[60px] flex items-center justify-between"
@@ -20,8 +18,8 @@ export function Header() {
           to="/"
           className="flex items-center gap-2 no-underline"
         >
-          <img src={Logo} width={50} alt="" />
-          <span className="font-bold text-base text-[#0f172a]">
+          <img src={Logo} width={70} alt="" />
+          <span className="font-bold text-lg text-[#0f172a]">
             WriteReady <span className="text-[#c9900a]">IELTS</span>
           </span>
         </Link>
@@ -41,17 +39,17 @@ export function Header() {
           {user ? (
             <Link
               to="/account"
-              className="ml-2 flex items-center gap-2 bg-[#1e3a5f] text-white text-sm font-semibold pl-1.5 pr-4 py-1.5 rounded-lg no-underline"
+              className="ml-2 flex hover:bg-[#1e3a5f]/90 items-center gap-2 bg-[#1e3a5f] text-white text-sm font-semibold px-3 py-2 rounded-lg no-underline"
             >
               {user.photoURL ? (
-                <img
+                <img width={30}
                   src={user.photoURL}
-                  alt={firstName}
-                  className="w-7 h-7 rounded-full object-cover shrink-0"
+                  alt={firstName as any}
+                  className=" rounded-full object-cover shrink-0"
                 />
               ) : (
                 <span className="w-7 h-7 rounded-full bg-white/20 flex items-center justify-center text-xs font-bold shrink-0">
-                  {firstName.charAt(0).toUpperCase()}
+                  {firstName}
                 </span>
               )}
               {firstName}
